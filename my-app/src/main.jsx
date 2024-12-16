e6819345c1f17d8a3201414380fb2a6079e76263
maulanamb <mmbhaswara@gmail.com>
2024-12-17 01:30:48 +0700

    Update main.jsx

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import './file_css/Index.css';
 
 // Membuat root React dan merender aplikasi dengan BrowserRouter
 ReactDOM.createRoot(document.getElementById('root')).render(
